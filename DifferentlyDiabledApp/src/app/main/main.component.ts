00d82d20910084481a4312de43e3daa4b2840572
amrir2240 <amri.r22402@gmail.com>
2021-06-27 11:21:24 -0500

    footer and card

--- DifferentlyDiabledApp/src/app/main/main.component.ts
+++ DifferentlyDiabledApp/src/app/main/main.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  title: string = '';
+  description: string = '';
+  buttonState: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.title = 'Cricket'
+    this.description  = 'Cricket is a bat-and-ball game played between two teams of eleven players on a field at the centre of which is a 22-yard (20-metre) pitch with a wicket at each end, each comprising two bails balanced on three stumps. ... When ten batters have been dismissed,'
+    +'the innings ends and the teams swap roles.';
+    this.buttonState = true;
   }
 
 }
